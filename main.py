b3db5a498285f35e1513f50399f55fff310f301c
jjackson112 <jjackson11211@gmail.com>
2025-04-03 11:49:24 -0400

    To Do List while loop

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ for years in age_list:
   if years > 9:
     num_eligible = num_eligible + 1
   print(num_eligible)
+
+# T0 Do List exercise
+todo_list = []
+done = False
+
+print("Type 'done' at any time to exit")
+
+# setting the loop to not done means it will run as long as done is not True
+while not done:
+  new_item = input("Add an item to your to do list:")
+  if new_item == "done":
+    done = True
+    print(todo_list)
+  else:
+    todo_list.append(new_item)
